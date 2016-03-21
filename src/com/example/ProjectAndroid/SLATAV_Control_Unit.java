d01599c3c543d8f46a6ad9a99f0c0e988484a6c8
BlackBull2015 <aroslaw007chudy@gmail.com>
2016-03-21 18:51:08 +0000

    Working android app... Few final touches are required

--- src/com/example/ProjectAndroid/SLATAV_Control_Unit.java
+++ src/com/example/ProjectAndroid/SLATAV_Control_Unit.java
@@ -9,7 +9,9 @@ import android.content.pm.ActivityInfo;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Switch;
 import android.widget.Toast;
 
@@ -18,7 +20,7 @@ import java.io.OutputStream;
 import java.util.UUID;
 
 public class SLATAV_Control_Unit extends Activity {
-
+    
     BluetoothAdapter mBluetoothAdapter;
     BluetoothSocket runningsocket;
     BluetoothDevice masterdevice;
@@ -38,80 +40,129 @@ public class SLATAV_Control_Unit extends Activity {
         setContentView(R.layout.control);
         this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
         mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        Button  Forward = (Button) findViewById(R.id.button3);
+        Button Reverse = (Button)findViewById(R.id.button4);
+        Button  Left = (Button)findViewById(R.id.button);
+        Button Right = (Button)findViewById(R.id.button2);
+        Button Up = (Button)findViewById(R.id.button5);
+        Button Down = (Button)findViewById(R.id.button6);
+
+        Forward.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+                    Switch swt = (Switch)findViewById(R.id.switch1);
+
+                    if(swt.isChecked())
+                    outStream.write("forward".getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return false;
+            }
+        });
+
+        Reverse.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+                    Switch swt = (Switch)findViewById(R.id.switch1);
+
+                    if(swt.isChecked())
+                    outStream.write("reverse".getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return false;
+            }
+        });
+        Left.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+                    Switch swt = (Switch)findViewById(R.id.switch1);
+
+                    if(swt.isChecked())
+                    outStream.write("left".getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return false;
+            }
+        });
+
+        Right.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+                    Switch swt = (Switch)findViewById(R.id.switch1);
+
+                    if(swt.isChecked())
+                    outStream.write("right".getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return false;
+            }
+        });
+
+
+        Up.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+                    Switch swt = (Switch)findViewById(R.id.switch1);
+
+                    if(swt.isChecked())
+                    outStream.write("up".getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return false;
+            }
+        });
+
+
+        Down.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                try {
+                    Switch swt = (Switch)findViewById(R.id.switch1);
+
+                    if(swt.isChecked())
+                    outStream.write("down".getBytes());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+                return false;
+            }
+        });
+
+
+
+
     }
-//
-//    @Override
-//    protected void onDestroy() {
-//        // TODO Auto-generated method stub
-//        super.onDestroy();
-//        try {
-//            runningsocket.close();
-//        } catch (IOException e) {
-//            e.printStackTrace();
-//        }
-//    }
-
-
 
     public void Connect (View v){
         Log.v(TAG, "Could not send it streem");
         new backgroundAsyncTask().execute("");
+        Toast.makeText(this,"Establishing Connection", Toast.LENGTH_SHORT).show();
 
     }
 
-    public void forward (View v) throws IOException {
-
-        Switch swt = (Switch)findViewById(R.id.switch1);
-
-        if(swt.isChecked()) {
-            outStream.write("forward".getBytes());
-        }
-    }
-    public void revers (View v) throws IOException {
-
-        Switch swt = (Switch)findViewById(R.id.switch1);
-
-        if(swt.isChecked()) {
-            outStream.write("reverse".getBytes());
-        }
-    }
-    public void left (View v) throws IOException {
-
-
-        Switch swt = (Switch)findViewById(R.id.switch1);
-
-        if(swt.isChecked()) {
-            outStream.write("left".getBytes());
-        }
-    }
-    public void right(View v) throws IOException {
-
-        Switch swt = (Switch)findViewById(R.id.switch1);
-
-        if(swt.isChecked()) {
-            outStream.write("right".getBytes());
-        }
-    }
-    public void down (View v) throws IOException {
-
-        Switch swt = (Switch)findViewById(R.id.switch1);
-
-        if(swt.isChecked()) {
-            outStream.write("down".getBytes());
-        }
-    }
-    public void up (View v) throws IOException {
-
-        Switch swt = (Switch)findViewById(R.id.switch1);
-
-        if(swt.isChecked()) {
-            outStream.write("up".getBytes());
-        }
-    }
-    public void switchonoff (View v) {
+    public void switchonoff (View v) throws IOException {
 
         Switch swt = (Switch)findViewById(R.id.switch1);
         Context context = getApplicationContext();
+
+        outStream.write((swt.isChecked() ? "on" : "off").getBytes());
         Toast toast = Toast.makeText(context, swt.isChecked() ? "on" : "off", Toast.LENGTH_SHORT);
         toast.show();
     }
@@ -132,15 +183,18 @@ public class SLATAV_Control_Unit extends Activity {
         }
     }
 
-
-
-
-
     private class backgroundAsyncTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
 
         @Override
         protected String doInBackground(String... params) {
             try {
+
+
                 Log.e(TAG, "Getting device");
                 masterdevice = mBluetoothAdapter.getRemoteDevice(address);
                 Log.e(TAG, "Getting socket");
@@ -148,6 +202,7 @@ public class SLATAV_Control_Unit extends Activity {
                 Log.e(TAG, "Attempting to connecto to socket");
                 runningsocket.connect();
                 Log.e(TAG, "Getting out strem");
+
                 outStream = runningsocket.getOutputStream();
                 Log.e(TAG, "Wtiring to output stream");
                 outStream.write("Hello world".getBytes());
@@ -161,8 +216,4 @@ public class SLATAV_Control_Unit extends Activity {
         }// doInBackground
 
     }// backgroundAsyncTask
-
-
-
-
 }
